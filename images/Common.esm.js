aa8ea20d955ae9f890a0d6a10e354e8e66f9db56
Marcin Kosecki <marcin.kosecki@int.pl>
2022-03-31 18:14:19 +0200

    create class Common

---
+++ images/Common.esm.js
@@ -0,0 +1,18 @@
+export class Common {
+    constructor(elementId) {
+        if (typeof elementId === 'undefined') {
+            return
+        };
+
+        this.element = this.#bindElement(elementId)
+    };
+
+    #bindElement(elementId) {
+        const element = document.getElementById(elementId);
+        if (!element) {
+            throw new Error(`${element} doesn't exist`)
+        }
+        return element;
+    }
+}
+
