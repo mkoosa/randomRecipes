e54ba3fa0e48b2f7f038b6ace41f2313a6decd03
Marcin Kosecki <marcin.kosecki@int.pl>
2022-04-04 07:39:38 +0200

    remove id parameter from createHtmlElement in FrontRecipe.esm.js

--- scripts/FrontRecipe.esm.js
+++ scripts/FrontRecipe.esm.js
@@ -1,18 +1,8 @@
-const FRONT_CLASS = 'front';
-const FRONT_ID = 'FrontId'
-
+export const FRONT_CLASS = 'front';
 const FRONT_HEADING_CLASS = 'front__heading';
-const FRONT_HEADING_ID = 'frontHeadingId';
-
 const FRONT_BTN_CLASS = 'front__button';
-const FRONT_BTN_ID = 'frontButtonID';
-
 const FRONT_ICONS_CLASS = ['fa-solid', 'fa-arrow-up-right-from-square'];
-const FRONT_ICON_ID = 'frontIconId';
-
 const FRONT_PARAGRAPH_CLASS = 'front__paragraph';
-const FRONT_PARAGRAPH_ID = 'frontParagraphId';
-
 
 const DIV = 'div';
 const H2 = 'h2';
@@ -26,20 +16,18 @@ export class FrontRecipe {
     };
 
     createFrontRecipeHTMLElements() {
-        this.recipeFront = this.createHtmlElement(DIV, FRONT_CLASS, FRONT_ID);
-        this.frontHeading = this.createHtmlElement(H2, FRONT_HEADING_CLASS, FRONT_HEADING_ID);
-        this.frontBtn = this.createHtmlElement(DIV, FRONT_BTN_CLASS, FRONT_BTN_ID);
+        this.recipeFront = this.createHtmlElement(DIV, FRONT_CLASS);
+        this.frontHeading = this.createHtmlElement(H2, FRONT_HEADING_CLASS);
+        this.frontBtn = this.createHtmlElement(DIV, FRONT_BTN_CLASS);
         this.frontIcon = document.createElement(I);
         this.createClassesInIconElement(this.frontIcon);
-        this.frontIcon.setAttribute('id', FRONT_ICON_ID);
-        this.frontParagraph = this.createHtmlElement(P, FRONT_PARAGRAPH_CLASS, FRONT_PARAGRAPH_ID);
+        this.frontParagraph = this.createHtmlElement(P, FRONT_PARAGRAPH_CLASS);
 
     };
 
-    createHtmlElement(element, className, idName) {
+    createHtmlElement(element, className) {
         const domElement = document.createElement(element);
         domElement.classList.add(className);
-        domElement.setAttribute('id', idName);
         return domElement;
 
     };
@@ -53,5 +41,3 @@ export class FrontRecipe {
     }
 
 }
-
-console.log('ok')
