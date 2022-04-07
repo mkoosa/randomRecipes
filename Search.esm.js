38dd14df6d678eb48f3be453cfd38971c674f048
Marcin Kosecki <marcin.kosecki@int.pl>
2022-04-07 12:39:04 +0200

    add changeLoupeColor in Search.esm.js

--- Search.esm.js
+++ Search.esm.js
@@ -1,18 +1,43 @@
-import { Common } from "./scripts/Common.esm.js";
+import {
+    Common
+} from "./scripts/Common.esm.js";
 
 const SEARCH_INPUT_ID = 'searchInput';
+const LOUPE_ID = 'loupe';
 
 
 
 
-class Search extends Common{
+class Search extends Common {
     constructor() {
         super();
         this.searchInput = this.bindElement(SEARCH_INPUT_ID);
+        this.loupe = this.bindElement(LOUPE_ID);
+        this.getSearchingRecipe();
+    }
+    
+    
+    getSearchingRecipe() {
+        this.searchInput.addEventListener('keyup', (e) => {
+            
+            let value = e.target.value
+            
+            
+            
+            this.changeLoupeColor(value);
+        })
+
     }
 
-
-
+    changeLoupeColor(value) {
+        if (value !== '') {
+            this.loupe.style.color = 'red';
+        }
+        else {
+            
+            this.loupe.style.color = '#fff';
+        }
+    }
 
 }
 
@@ -21,4 +46,3 @@ class Search extends Common{
 
 
 export const search = new Search();
-console.log(search);
