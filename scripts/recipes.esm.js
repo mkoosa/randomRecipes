a6ffbc3afee3f0ad7d4c7968018b73d434332e41
Marcin Kosecki <marcin.kosecki@int.pl>
2022-04-16 17:29:32 +0200

    rebuild class Recipes in recipes.esm.js

--- scripts/recipes.esm.js
+++ scripts/recipes.esm.js
@@ -6,66 +6,62 @@ import {
     FrontRecipe
 } from "./FrontRecipe.esm.js";
 
-
-import { FRONT_CLASS } from "./FrontRecipe.esm.js";
-
+import {
+    FRONT_CLASS
+} from "./FrontRecipe.esm.js";
 
 const RECIPES_ID = 'recipes';
 const URL = 'https://themealdb.com/api/json/v1/1/random.php';
 const RECIPES_AMOUNT = 7;
 
-
 export class Recipes extends Common {
     constructor() {
         super();
         this.recipes = this.bindElement(RECIPES_ID);
-        this.createRandomRecipes();
+        this.createRandomRecipes(RECIPES_AMOUNT);
         this.createNrOfIdFrontRecipe();
         this.randomRecipesDetails = [];
-        
-    }
-
-    createRandomRecipes() {
-        for (let i = 0; i < RECIPES_AMOUNT; i++) {
+    };
+    
+    createRandomRecipes(amount) {
+        for (let i = 0; i < amount; i++) {
             this.createRandomRecipe();
-        }
-    }
-
+        };
+    };
+    
     createNrOfIdFrontRecipe() {
         const recipes = document.querySelectorAll(`.${FRONT_CLASS}`);
         recipes.forEach((div, i) => {
             div.setAttribute('id', `FrontId-${i}`);
-        })
-    }
+        });
+    };
 
     createRandomRecipe() {
         this.createFrontRecipeHtmlElements();
-    }
+    };
 
     createFrontRecipeHtmlElements() {
+        let frontRecipe = this.createFrontRecipeObj();
+        this.getRandomData(frontRecipe); 
+    };
+    
+    createFrontRecipeObj() {
         const frontRecipe = new FrontRecipe();
-
-        if (!frontRecipe) {
-            throw new Error(`${frontRecipe} doesn't exist`)
-        }
-
         const {
             recipeFront,
             frontHeading,
             frontBtn,
             frontIcon,
             frontParagraph,
-
         } = frontRecipe;
-
+    
         const front = this.recipes.appendChild(recipeFront);
         front.appendChild(frontHeading);
         const btn = front.appendChild(frontBtn);
-       
         btn.appendChild(frontIcon);
         btn.appendChild(frontParagraph).textContent = 'open recipe';
-        this.getRandomData(frontRecipe);
-    }
+        return frontRecipe;
+    };
 
     getDishName(element, dishName) {
         element.textContent = dishName;
@@ -73,33 +69,19 @@ export class Recipes extends Common {
 
     getRandomData(frontElements) {
         fetch(URL)
-        .then((res) => res.json())
-        .then(data => {
-            
-            const recipeDetails = data.meals[0]
-            const {
-                    strMealThumb,
-                    strInstructions,
-                    strMeal
-                } = recipeDetails;
+            .then((res) => res.json())
+            .then(data => {
+                const recipeDetails = data.meals[0];
                 this.randomRecipesDetails.push(recipeDetails);
-                
-                const {
-                    frontHeading,
-                    
-                } = frontElements;
-                
-                this.getDishName(frontHeading, strMeal);
+                this.getDishName(frontElements.frontHeading, recipeDetails.strMeal);
                 this.saveRecipesInLocalStorage();
             })
             .catch(error => console.error('Error:', error));
     }
-    
-        saveRecipesInLocalStorage() {
-            localStorage.setItem('array', JSON.stringify(this.randomRecipesDetails));
-            
-        }
-}
 
- const recipes = new Recipes();
+    saveRecipesInLocalStorage() {
+        localStorage.setItem('array', JSON.stringify(this.randomRecipesDetails));
+    };
+};
 
+export const recipes = new Recipes();
