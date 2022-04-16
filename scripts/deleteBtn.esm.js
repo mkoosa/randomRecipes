691739a98b4b91642202937beaa4a6d4a38ed5a5
Marcin Kosecki <marcin.kosecki@int.pl>
2022-04-16 10:05:20 +0200

    create deleteBtn.esm.js and add getBtn, removeRecipe, changeValuesInStorage in the same file

---
+++ scripts/deleteBtn.esm.js
@@ -0,0 +1,42 @@
+import { Common } from "./Common.esm.js";
+import { DELETE_BTN_ID } from "./MainRecipe.esm.js";
+import { openedRecipe, KEY_STORAGE } from "../OpenedRecipe.esm.js";
+import { MAIN_ID } from "./MainRecipe.esm.js";
+
+
+const deleteBtn = 'deleteBtn';
+
+export class DeleteBtn extends Common {
+    constructor(number) {
+        super()
+        this.number = number;
+        this.main = this.bindElement(MAIN_ID);
+        this.openedRecipe = openedRecipe;
+        this.getBtn(deleteBtn, DELETE_BTN_ID)
+    }
+    
+    getBtn(element, id) {
+        this.element = this.bindElement(id);
+        this.element.addEventListener('click', () => this.removeRecipe());
+        
+    }    
+    removeRecipe() {
+        let recipes = JSON.parse(localStorage.getItem(KEY_STORAGE));
+        recipes.splice(this.number, 1);
+        this.changeValueInStorage(recipes);
+        this.main.remove();
+        // console.log(recipes);
+    }
+    
+    
+    changeValueInStorage(value) {
+        localStorage.clear();
+        localStorage.setItem('array', JSON.stringify(value));
+        
+    }
+
+  
+
+}
+
+
