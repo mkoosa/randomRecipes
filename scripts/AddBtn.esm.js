dd2545b2395554b2c9e16dd8ef792c1f72bbc3ae
Marcin Kosecki <marcin.kosecki@int.pl>
2022-05-24 10:37:03 +0200

    corrected prepareCorrectForm, prepareInput. prepareTextArea, displayCorrectForm in AddBtn.esm.js

--- scripts/AddBtn.esm.js
+++ scripts/AddBtn.esm.js
@@ -21,10 +21,12 @@ const FORM_CLASS = 'myRecipe__form';
 const LABEL_CLASS = 'myRecipe__label';
 const INPUT_CLASS = 'myRecipe__input';
 const P_CLASS = 'myRecipe__btn';
+const ADD_CLASS = 'myRecipe__AddBtn';
 const ICON__CLASSES = ['fa-solid', 'fa-square-check'];
 const CLOSE_ICON_CLASSES = ['fa-solid', 'fa-x', 'myRecipe__close']
 const TEXTAREA_CLASS = 'textArea';
 const SAVE = 'save';
+const ADD = 'add';
 const ADD_BTN_ID = 'addBtn';
 const MY_RECIPE_ID = 'myRecipe';
 const WRAPPER_ID = 'wrapperId'
@@ -32,31 +34,29 @@ const WRAPPER_ID = 'wrapperId'
 class AddBtn extends Common {
     constructor() {
         super();
+
         this.target = 0;
+        this.flag = false;
         this.addButton = this.bindElement(ADD_BTN_ID);
         this.wrapper = this.bindElement(WRAPPER_ID);
-        this.descriptions = ['name of dish', 'INGREDIENTS', 'Preparation:'];
+        this.descriptions = ['name of dish', 'INGREDIENTS', 'Preparation:', 'add image'];
         this.mainRecipe = mainRecipe;
-        this.eventTargets();
+        this.addButton.addEventListener('click', () => this.openMyRecipeForm());
     };
 
-    prepareRecipeDetails() {
-        this.myDish = new MyDish(this.saveInputValue);
-        console.log(this.myDish);
-        this.createFormHTMLElements();
-    };
+    openMyRecipeForm() {
+        this.prepareInput();
+        this.myDish = new MyDish();
+    }
 
-    createFormHTMLElements() {
+    prepareCorrectForm() {
         this.myRecipe = this.mainRecipe.frontRecipe.createHtmlElement(DIV, DIV_CLASS);
         this.mainRecipe.createIdAttribute(this.myRecipe, MY_RECIPE_ID);
         this.form = this.mainRecipe.frontRecipe.createHtmlElement(FORM, FORM_CLASS);
         this.label = this.mainRecipe.frontRecipe.createHtmlElement(LABEL, LABEL_CLASS);
         this.label.textContent = this.descriptions[this.target];
-        this.input = this.mainRecipe.frontRecipe.createHtmlElement(INPUT, INPUT_CLASS);
-        this.input.type = INPUT_TYPE;
         this.p = this.mainRecipe.frontRecipe.createHtmlElement(P, P_CLASS);
         this.icon = document.createElement(ICON);
-        this.textArea = this.mainRecipe.frontRecipe.createHtmlElement(TEXT_AREA, TEXTAREA_CLASS);
         this.p.textContent = SAVE;
         this.mainRecipe.frontRecipe.createClassesInElement(this.icon, ICON__CLASSES);
         this.closeIcon = document.createElement(ICON);
@@ -65,78 +65,78 @@ class AddBtn extends Common {
         this.myRecipe.appendChild(this.form);
         this.myRecipe.appendChild(this.closeIcon);
         this.form.appendChild(this.label);
-        this.form.appendChild(this.input);
         this.form.appendChild(this.p);
         this.p.appendChild(this.icon);
-        this.p.addEventListener('click', () => this.displayCorrectForm());
+    };
+
+    prepareInput() {
+        this.prepareCorrectForm();
+        this.input = this.mainRecipe.frontRecipe.createHtmlElement(INPUT, INPUT_CLASS);
+        this.input.type = INPUT_TYPE;
+        this.form.appendChild(this.input);
+        this.p.addEventListener('click', () => this.displayCorrectForm(this.input));
         this.closeIcon.addEventListener('click', () => this.closeForm());
+    }
+
+    prepareTextArea() {
+        if (this.target === this.descriptions.length) {
+            this.target = 0;
+            return
+        };
+        this.prepareCorrectForm();
+        console.log(this.target)
+        if (this.target === this.descriptions.length - 1) {
+            console.log('image');
+            this.prepareImage();
+            return;
+        }
+        this.textArea = this.mainRecipe.frontRecipe.createHtmlElement(TEXT_AREA, TEXTAREA_CLASS);
+        this.form.appendChild(this.textArea);
+        this.p.addEventListener('click', () => this.displayCorrectForm(this.textArea));
+        this.closeIcon.addEventListener('click', () => this.closeForm());
+    }
+
+    prepareImage() {
+        this.p = this.mainRecipe.frontRecipe.createHtmlElement(P, ADD_CLASS);
+        this.p.textContent = ADD;
+        this.p.appendChild(this.icon);
+        this.form.appendChild(this.p);
+        this.closeIcon.addEventListener('click', () => this.closeForm());
+    }
+
+    displayCorrectForm(element) {
+        this.fillMyDish(this.target, element.value)
+        if (this.isValueEmpty(element)) {
+            this.myRecipe.remove();
+            this.target++;
+            this.prepareTextArea();
+        };
     };
 
-    getValue(element) {
-        return element.value;
+    isValueEmpty(element) {
+        return element.value || element.value.trim();
     };
 
-    displayCorrectForm() {
-        const elements = [this.input, this.textArea, this.textArea];
-        const element = elements[this.target];
-        this.checkTarget(elements);
-        this.isEmptyValue(element);
+    closeForm() {
+        this.myRecipe.remove();
+        this.target = 0;
     };
 
     fillMyDish(target, value) {
+        console.log(target, value);
         switch (target) {
             case 0:
                 this.myDish.setName = value;
                 break;
             case 1:
-                this.myDish.strIngredient = value;
+                this.myDish.setIngredients = value;
                 break;
             case 2:
                 this.myDish.setInstructions = value;
                 break;
         };
+        console.log(this.myDish);
     };
-
-    closeForm() {
-        this.form.remove();
-        this.myRecipe.style.display = 'none';
-        this.target = 0;
-    }
-
-    checkTarget(elements) {
-        if (this.target === elements.length - 1) {
-            this.form.remove();
-            this.myRecipe.style.display = 'none';
-            this.target = 0;
-            return;
-        };
-    };
-
-    isEmptyValue(element) {
-        if (this.getValue(element)) {
-            this.target++;
-            console.log(element.value)
-            this.fillMyDish(this.target, element.value);
-            console.log(this.myDish);
-            this.clearHtmlElement(element);
-            this.changeHtmlElement();
-            element.value = '';
-        };
-    };
-
-    clearHtmlElement(element) {
-        element.remove();
-    };
-
-    changeHtmlElement() {
-        this.label.textContent = this.descriptions[this.target];
-        this.myRecipe.style.height = '65vh';
-        this.form.appendChild(this.textArea);
-    };
-
-    eventTargets() {
-        this.addButton.addEventListener('click', () => this.prepareRecipeDetails())
-    };
-};
+}
 
 export const addBtn = new AddBtn();
